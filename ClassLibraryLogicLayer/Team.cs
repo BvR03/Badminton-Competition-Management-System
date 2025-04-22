b7b12d0b760f19bf0e81c6e0f320a6a12dcfe97a
BvR03 <thebvrsquad@gmail.com>
2025-04-22 13:13:56 +0200

    Updated TeamCollection

---
+++ ClassLibraryLogicLayer/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    class Team
+    {
+        private string Name;
+        public Team(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
--- ClassLibraryLogicLayer/TeamCollection.cs
+++ ClassLibraryLogicLayer/TeamCollection.cs
@@ -8,5 +8,16 @@ namespace LogicLayer
 {
     class TeamCollection
     {
+        private List<Team> teams = new List<Team>();
+        public void CreateTeam(string name)
+        {
+            Team team = new Team(name);
+
+        }
+        public List<Team> GetAllTeams()
+        {
+            return teams;
+        }
+
     }
 }
