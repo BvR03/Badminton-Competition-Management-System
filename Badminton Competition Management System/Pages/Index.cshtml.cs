7c9c48eba1cbbc0aa06e37e4cf846b7482be8d19
BvR03 <thebvrsquad@gmail.com>
2025-04-26 20:12:52 +0200

    Small edits
    - changed title to Main view
    
    planning to learn the html stuff soon

--- Badminton Competition Management System/Pages/Index.cshtml.cs
+++ Badminton Competition Management System/Pages/Index.cshtml.cs
@@ -5,7 +5,6 @@ namespace Badminton_Competition_Management_System.Pages;
 
 public class IndexModel : PageModel
 {
-
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
